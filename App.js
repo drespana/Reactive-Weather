88c3f796155e8b1615a9c0e3b2d9877e6cc896d2
drespana <drespana@google.com>
2023-10-20 17:32:42 -0500

    WeatherCard.js and App.js changes

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // Import data and WeatherCard here
-
+import WeatherCard from "./components/WeatherCard"
+import cities from "./data"
 
 function App() {
     return (
@@ -8,10 +9,11 @@ function App() {
             <h1 className = "title">REACTIVE WEATHER</h1>
             <h3 className = "subtitle">Up to the minute weather news</h3>
             <div className = "app">
-                {/* Render components here */}
-                
-
-
+                {
+                    cities.map((city,idx) => (
+                        <WeatherCard key={idx} data={city}/>
+                    ))                    
+                }
             </div>
             
         </>
--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,20 +1,30 @@
 import React from "react";
 import sunny from "../assets/Sunny.svg"
+import rainy from "../assets/Rainy.svg"
+import cloudy from "../assets/Cloudy.svg"
+import partlyCloudy from "../assets/PartlyCloudy.svg"
+const images = {
+  "Sunny": sunny,
+  "Rainy": rainy,
+  "Cloudy": cloudy,
+  "Partly cloudy": partlyCloudy
+}
 
-
-function WeatherCard() {
+function WeatherCard({data}) {
+  const {city, tempurature, forecast} = data;
   return (
     <div className = "card">
         <div className = "img-container">
-            <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
+            <img className="card-img-top" src = {images[forecast]} alt="Card image cap" id = "icon"/>
         </div>
         <div class="card-body">
-            <h3 className="card-title">CITY DATA HERE</h3>
-            <h5 className="card-text">TEMPERATURE DATA HERE</h5>
-            <h5 className="card-text">FORECAST DATA HERE</h5>
+            <h3 className="card-title">{city}</h3>
+            <h5 className="card-text">{tempurature}</h5>
+            <h5 className="card-text">{forecast}</h5>
         </div>
     </div>
   );
 };
 
 // Export the WeatherCard
+module.exports = WeatherCard;
